db23d84689b15b4c20d8ea657896408d85e3755e
guferut <guferut044@gmail.com>
2024-01-24 18:55:13 +0200

    add css styles

--- modal.js
+++ modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnOpenn = document.querySelector('.modal-btn-opena');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
+modalBtnOpenn.addEventListener('click', toggleModal)
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
